8da9ed7a419a8ff387c1cf5a3211702a6e985047
YoungY620 <no@thankyou.com>
2023-08-10 20:31:41 +0800

    build success

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,29 @@
-use std::{collections::HashMap, ops::Index, default};
+use std::result::Result::Ok;
 
-use anyhow::Ok;
 use discord_flows::{
     model::Message,
     ProvidedBot, Bot,
 };
 use flowsnet_platform_sdk::logger;
+use serde::Serialize;
 use serde_json::json;
-use store_flows::{get, set, Expire, ExpireKind};
-use rand::seq::{SliceRandom, thread_rng};
+use store_flows::{get, set, del};
+use rand::{seq::SliceRandom, thread_rng};
 
-static WINNER_MSG:String = String::from("Haha! I can't believe it! Looks like I completely crushed you, 
+static WINNER_MSG: &str = "Haha! I can't believe it! Looks like I completely crushed you, 
                             Don't worry, though, it's all in good fun. Maybe next time you'll stand a 
-                            chance against my unbeatable skills. Until then, enjoy the taste of defeat!");
-static LOSER_MSG: String = String::from("Alright, alright, you got me this time! 
+                            chance against my unbeatable skills. Until then, enjoy the taste of defeat!";
+static LOSER_MSG: &str = "Alright, alright, you got me this time! 
                             I'll begrudgingly admit it, my friend you beat me fair and square. 
-                            I'll be back for a rematch soon. Consider yourself lucky, my friend!");
-static TIE_MSG: String = String::from("Ha! It's a tie, my friend! I must say, it's quite a 
-                            rare occurrence. Shall we go at it again? ");
-static HELP_MSG: String = String::from("There are 3 options for you: \n
+                            I'll be back for a rematch soon. Consider yourself lucky, my friend!";
+static TIE_MSG: &str = "Ha! It's a tie, my friend! I must say, it's quite a 
+                            rare occurrence. Shall we go at it again? ";
+static HELP_MSG: &str = "There are 3 options for you: \n
                             1. By saying \"hit\", you can have another card\n
                             2. By saying \"stand\", you will take no more cards, so I will reveal the result.\n
                             3. By saying \"status\", you can know what cards in your hand as well as my face-up card.
-                            4. By saying \"help\" or something else, you will see this help message.");
-static INTRO_MSG: String = String::from("Let me introduce the rule of this game for you:\n
+                            4. By saying \"help\" or something else, you will see this help message.";
+static INTRO_MSG: &str = "Let me introduce the rule of this game for you:\n
                             1. First I will give each of us two cards and one of mine is face-down;\n
                             2. After that, you can take another card by saying \"hit\" for many times 
                             until you stop by saying \"stand\". \n
@@ -31,82 +31,89 @@ static INTRO_MSG: String = String::from("Let me introduce the rule of this game 
                             4. After you \"stand\", I will reveal the face-down card and take card until my point goes beyond 17. \n
                             5. If your point is less than 21 and greater than mine, or my point is greater than 21, you are the winner. \n
                             6. If my point is less than 21 and greater than yours, I win. (You will lose in step 3 if yours is greater than 21.)\n
-                            7. In other situations, it's a tie.");
+                            7. In other situations, it's a tie.";
 
 type Card = String;
 
+#[derive(Serialize)]
 struct Game{
     pub dealer_cards: Vec<Card>,
     pub player_cards: Vec<Card>,
 
-    card2use: Vec<String>
+    card2use: Vec<Card>
 }
 
 impl Game {
-    fn sum_of(&self, cards: &Vec<char>) -> u64{
-        let mut sum:i64 = 0;
+    fn sum_of(&self, cards: &Vec<Card>) -> u64{
+        let mut sum = 0;
         for c in cards {
-            let v = match c {
-                "2"|"3"|"4"|"5"|"6"|"7"|"8"|"9" => c.to_digit(10).unwrap(),
+            let v = match c.as_str() {
+                "2"|"3"|"4"|"5"|"6"|"7"|"8"|"9" => c.parse::<u64>().unwrap(),
                 "10"|"J"|"Q"|"K" => 10,
-                'A' => 11
+                "A" => 11,
+                &_ => panic!("Invalid Card Name")
             };
-            sum += c;
+            sum += v;
         }
         sum
     }
-    fn pop_one_card(&self) -> Result<u8, String> {
-        if let Some(next) = self.card2use.first().clone(){
+    fn pop_one_card(&mut self) -> Result<Card, &'static str> {
+        if let Some(s) = self.card2use.first(){
+            let next = s.clone();
             self.card2use.remove(0);
             Ok(next)
         }else{
-            Err(String("Insufficient Cards"))
+            Err("Insufficient Cards")
         }
     }
-    pub fn init_game(&self) -> Result<(), String> {
+    pub fn init_game(&mut self) -> Result<(), &'static str> {
         for _ in 0..2{
-            self.player_cards.push(self.pop_one_card()?);
+            let card = self.pop_one_card()?;
+            self.player_cards.push(card);
         }
-        self.dealer_cards.push(self.pop_one_card()?);
+        let card = self.pop_one_card()?.clone();
+        self.dealer_cards.push(card);
         Ok(())
     }
 
     pub fn new() -> Game {
-        let one_deck = vec!["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-        let one_deck: Vec<String> = one_deck.iter().map(|&s|s.to_owned()).collect();
-        one_deck = Vec::new();
+        let quatar: Vec<String> = vec!["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+                                        .iter().map(|&s|s.to_owned()).collect();
+        let mut one_deck = Vec::new();
         for _ in 0..4 {
-            one_deck.extend(one_deck);
+            one_deck.extend(quatar.clone());
         }
-        one_deck.extend(one_deck);
         let mut rng = thread_rng();
         one_deck.shuffle(&mut rng);
 
         Game { dealer_cards: Vec::new(), player_cards: Vec::new(), card2use: one_deck }
     }
 
-    pub fn hit(&self) -> Result<bool, String>{
-        self.player_cards.push(self.pop_one_card()?);
-        if self.sum_of(cards) > 21{
+    pub fn hit(&mut self) -> Result<bool, &'static str>{
+        let card = self.pop_one_card()?;
+        self.player_cards.push(card);
+        if self.sum_of(&self.player_cards) > 21{
             return Ok(true)
         }
         Ok(false)
     }
 
-    pub fn stand(&self) -> Option<bool>{
-        self.dealer_cards.push(self.pop_one_card());
+    pub fn stand(&mut self) -> Result<Option<bool>, &'static str>{
+        let card = self.pop_one_card()?;
+        self.dealer_cards.push(card);
         while self.sum_of(&self.dealer_cards) < 17{
-            self.dealer_cards.push(self.pop_one_card());
+            let icard = self.pop_one_card()?;
+            self.dealer_cards.push(icard);
         }
 
         let dealer_score = self.sum_of(&self.dealer_cards);
         let player_score = self.sum_of(&self.player_cards);
         if player_score > 21 || player_score < dealer_score {
-            Some(false)
+            Ok(Some(false))
         }else if dealer_score < player_score || dealer_score > 21{
-            Some(true)
+            Ok(Some(true))
         }else {
-            None
+            Ok(None)
         }
     }
 
@@ -141,24 +148,33 @@ async fn handler(bot: &ProvidedBot, msg: Message) {
     }
 
     if let Some(state_store) = get("bj"){
-        let game = Game{
-            player_cards: state_store.as_obj()["player_cards"].copy(),
-            dealer_cards: state_store.as_obj()["dealer_cards"].copy(),
-            card2use: state_store.as_obj()["card2use"].copy()
+        let mut game = Game{
+            player_cards: state_store["player_cards"].as_array().unwrap().clone().iter_mut().map(|v|String::from(v.as_str().unwrap())).collect(),
+            dealer_cards: state_store["dealer_cards"].as_array().unwrap().clone().iter_mut().map(|v|String::from(v.as_str().unwrap())).collect(),
+            card2use: state_store["card2use"].as_array().unwrap().clone().iter_mut().map(|v|String::from(v.as_str().unwrap())).collect()
         };
-        let resp = match msg.content.to_lowercase().trim() {
+        let mut endding = false;
+        let mut resp = match msg.content.to_lowercase().trim() {
             "hit" => match game.hit(){
                     Ok(true) => String::from("Order received."),
-                    Err(e) => format!("There's something wrong and the game will be terminated. Possible Reason: {}", e)
+                    Ok(false) => {endding = true; String::from(WINNER_MSG)},
+                    Err(e) => {
+                        endding = true;
+                        format!("There's something wrong and the game will be terminated. Possible Reason: {}", e)
+                    }
                 },
-            "stand" => match game.stand() {
-                Some(true) => LOSER_MSG,
-                Some(false) => WINNER_MSG,
-                None => TIE_MSG
+            "stand" => {
+                endding = true; 
+                match game.stand() {
+                    Ok(Some(true)) => String::from(LOSER_MSG),
+                    Ok(Some(false)) => String::from(WINNER_MSG),
+                    Ok(None) => String::from(TIE_MSG),
+                    Err(e) => format!("There's something wrong and the game will be terminated. Possible Reason: {}", e)
+                }
             },
-            "help" => HELP_MSG,
-            "status" => &game.status(),
-            default => format!("I don't know what you mean. \n{}", HELP_MSG)
+            "help" => String::from(HELP_MSG),
+            "status" => game.status(),
+            _ => format!("I don't know what you mean. \n{}", String::from(HELP_MSG))
         };
         resp = format!("{}\n Current Status: \n{}", resp, game.status());
         let channel_id = msg.channel_id;
@@ -170,17 +186,34 @@ async fn handler(bot: &ProvidedBot, msg: Message) {
             }),
         ).await;
 
+        if endding{
+            del("bj");
+        }else{
+            set("bj", json!(game), None);
+        }
+    }else {
+        if msg.content.to_lowercase().trim() != "blackjack"{
+            log::debug!("ignored channel message");
+            return;
+        }
+        let mut game = Game::new();
+        let resp = match game.init_game(){
+            Ok(_) => format!("Ok, let's begin. \n{}\nFor now,\n{}\n{}", INTRO_MSG, game.status(), HELP_MSG),
+            Err(e) => format!("There's something wrong and the game will be terminated. Possible Reason: {}", e)
+        };
+        let channel_id = msg.channel_id;
+
+        _ = discord.send_message(
+            channel_id.into(),
+            &serde_json::json!({
+                "content": resp
+            }),
+        ).await;
+
         set(
             "bj", json!(game), None
         );
-    }else {
-        if msg.content.to_lowercase().trim() == "blackjack"{
-            let game = Game::new();
-            match game.init_game(){
-                Ok(_) => format!("Ok, let's begin. \n{}\nFor now,\n{}\n{}", INTRO_MSG, game.status(), HELP_MSG),
-                Err(e) => format!("There's something wrong and the game will be terminated. Possible Reason: {}", e)
-            }
-        }
     }
+    
 
 }
